f3dc5010a141f131f5b64ace50d931ed6010c59c
GUILLOT Etienne <etienne.guillot@soprasteria.com>
2023-07-27 10:44:00 +0200

    Génération de la grid via une fonction

--- src/Components/Pages/Map/HexagonGrid.tsx
+++ src/Components/Pages/Map/HexagonGrid.tsx
@@ -1,67 +1,56 @@
-import { Line, RegularPolygon } from 'react-konva';
+import { RegularPolygon } from 'react-konva';
+
+type Hexagon = {
+  x: number;
+  y: number;
+};
 
 function HexagonGrid() {
-  const RADIUS = 30;
-  const diffX = Math.ceil(Math.sqrt(3 * RADIUS * RADIUS)) + 1;
+  const RADIUS = 40;
+  const diffX = Math.ceil(Math.sqrt(3 * RADIUS * RADIUS));
   const diffY = (RADIUS * 3) / 2;
+
+  const computeHexagons = (width: number, height: number) => {
+    let hexagonGrid: Hexagon[][] = [];
+    for (let i = -height; i <= height; ++i) {
+      const lineY = i * diffY;
+      let hexagonLine: Hexagon[] = [];
+      for (let j = -width; j <= width; ++j) {
+        let hexagonTile: Hexagon = {
+          x: 0,
+          y: lineY,
+        };
+        if (i % 2 === 0) {
+          hexagonTile.x = j * diffX;
+        } else {
+          hexagonTile.x = j * diffX + diffX / 2;
+        }
+        hexagonLine[j + width] = hexagonTile;
+      }
+      hexagonGrid[i + height] = hexagonLine;
+    }
+    return hexagonGrid;
+  };
+
+  const hexagons: Hexagon[][] = computeHexagons(8, 5);
+
   return (
     <>
-      <RegularPolygon sides={6} x={0} y={0} radius={30} fill='black' stroke='red' strokeWidth={1} />
-      <RegularPolygon
-        sides={6}
-        x={diffX}
-        y={0}
-        radius={RADIUS}
-        fill='black'
-        stroke='red'
-        strokeWidth={1}
-      />
-      <RegularPolygon
-        sides={6}
-        x={-diffX}
-        y={0}
-        radius={RADIUS}
-        fill='black'
-        stroke='red'
-        strokeWidth={1}
-      />
-
-      <RegularPolygon
-        sides={6}
-        x={diffX / 2}
-        y={diffY}
-        radius={30}
-        fill='black'
-        stroke='red'
-        strokeWidth={1}
-      />
-      <RegularPolygon
-        sides={6}
-        x={diffX / 2}
-        y={-diffY}
-        radius={30}
-        fill='black'
-        stroke='red'
-        strokeWidth={1}
-      />
-      <RegularPolygon
-        sides={6}
-        x={-diffX / 2}
-        y={diffY}
-        radius={30}
-        fill='black'
-        stroke='red'
-        strokeWidth={1}
-      />
-      <RegularPolygon
-        sides={6}
-        x={-diffX / 2}
-        y={-diffY}
-        radius={30}
-        fill='black'
-        stroke='red'
-        strokeWidth={1}
-      />
+      {hexagons.map((line) => {
+        return line.map((hexagon) => {
+          return (
+            <RegularPolygon
+              sides={6}
+              x={hexagon.x}
+              y={hexagon.y}
+              radius={RADIUS}
+              fill='black'
+              stroke='red'
+              strokeWidth={1}
+            />
+          );
+        });
+      })}
     </>
   );
 }
